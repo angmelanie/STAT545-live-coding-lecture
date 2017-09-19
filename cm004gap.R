69f8d05a84f2283eec8b401fa0fb8335339ce67c
Melanie Ang <melanieang07@gmail.com>
2017-09-19 14:55:52 -0700

    Commit from Tuesday lecture, Sept 19

---
+++ cm004gap.R
@@ -0,0 +1,12 @@
+# load packages
+install.packages("tidyverse")
+
+library(tidyverse)
+library(gapminder)
+
+str(gapminder)
+summary(gapminder)
+glimpse(gapminder)
+
+?glimpse
+install.packages("forcats")
---
+++ notes.R
@@ -0,0 +1,16 @@
+# use knit button to move .Rmd -> .md -> .html
+# knit creates or updates the html file
+# status: Add, Marked, Question
+
+
+# Sept 17 lecture
+# tidyverse <- dplyr and ggplot both within
+# filter: rows
+# select: columns
+# big data set useSQL
+
+# piping not into first position
+# x %>%
+#  f(arg1 = "something", arg2 = .)
+# . means the result of first pipe
+# does . mean the result of the last pipe
